e199cf143d3c12f24646b537c54267f1a87ffe46
Rohtash Lakra <rslakra@users.noreply.github.com>
2024-06-18 12:30:04 -0700

    updated linkGenerator script

--- linkGenerator.sh
+++ linkGenerator.sh
@@ -4,15 +4,16 @@
 #
 export CUR_DIR="${PWD}"
 export USER_HOME="${HOME}"
+export APPS_HOME="/Applications"
 export WORKSPACE_HOME="${USER_HOME}/Workspace"
 export SCRIPTS_HOME="${WORKSPACE_HOME}/Scripts"
-export GIT_HOME="${WORKSPACE_HOME}/VCS/Git"
+export GIT_HOME="${SCRIPTS_HOME}/VCS/Git"
 echo
 #echo "${WORKSPACE_HOME} not found or is symlink to $(readlink -f ${WORKSPACE_HOME})."
 
 # Check the Workspaces folder is under documents or not.
 if [ ! -d "${WORKSPACE_HOME}" -a ! -h "${WORKSPACE_HOME}" ]; then 
-    SCRIPTS_HOME="${SCRIPTS_HOME}"
+    SCRIPTS_HOME="${WORKSPACE_HOME}/Scripts"
 fi
 echo "SCRIPTS_HOME: ${SCRIPTS_HOME}"
 echo
@@ -21,11 +22,11 @@ ln -sf "${GIT_HOME}/removeBranch.sh" ~/removeGitBranch
 ln -sf "${GIT_HOME}/syncBranches.sh" ~/syncBranches
 ln -sf "${GIT_HOME}/tagBranch.sh" ~/tagGitBranch
 
-# Generate SyncroSVNClient Link
+# Generate syncroSVNClient Link
 # brew install openjdk@11
 # sudo ln -sfn /opt/homebrew/opt/openjdk@11/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-11.jdk
 #
-SYNCRO_SVN_CLIENT_HOME="/Applications/syncroSVNClient"
+SYNCRO_SVN_CLIENT_HOME="${APPS_HOME}/syncroSVNClient"
 ln -sf "${SYNCRO_SVN_CLIENT_HOME}/diffDirsMac.sh" ~/diffFolders
 
 # move back to current dir
