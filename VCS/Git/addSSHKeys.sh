31bf7bfe4f32af87b33389337df3ce8b00680602
Rohtash Lakra <rslakra@users.noreply.github.com>
2025-09-08 12:57:17 -0700

    updated script

--- VCS/Git/addSSHKeys.sh
+++ VCS/Git/addSSHKeys.sh
@@ -4,10 +4,8 @@ echo
 USER_NAME="${1}"
 if [ "${USER_NAME}" == "rslakra" ]; then
   ssh-add ~/.ssh/id_ed25519_rslakra
-elif [ "${USER_NAME}" == "tinker" ]; then
-  ssh-add ~/.ssh/id_ed25519
 else
-  ssh-add ~/.ssh/id_rsa
+  ssh-add ~/.ssh/id_ed25519
 fi
 
 echo
